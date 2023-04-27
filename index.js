ee77322cdab77471bc6604e9b56256708186d3de
Tomasz Zawadzki <103272474+ExpertoMontanero@users.noreply.github.com>
2023-04-27 23:07:06 +0200

    Update index.js
    Code specification added

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 function accum(s) {
+	//making an array from string
 	var temp =s.split("");
 	var array =[];
 	var j=0;
+	//main loop
 	for(var i=0; i<temp.length; i++)
 	{
+  	//first letter uppercase
 	    array.push(temp[i].toUpperCase())
+	//loop for how many items should be pushed
 	    for(var x=0; x<j; x++)
 	    {
 	        array.push(temp[i].toLowerCase());
 	    }
+	//"-" after every sequence
 	   array.push("-");
 	   j++;
 	}
+	//removing "-"
 	array.pop();
 	return array.join("");
 }
 
-
+//EXAMPLES:
 console.log(accum("ZpglnRxqenU"))
 console.log(accum("NyffsGeyylB"))
 console.log(accum("EvidjUnokmM"))
